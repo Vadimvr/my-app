ee6ce275bfacd28d5a9ebc4e0078c34137028f04
Вадим <r.vadim.90@gmail.com>
2022-05-02 23:56:53 +0300

    added Form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 // import ClassCounter from "./components/ClassCounter";
 // import Counter from "./components/Counter";
 import "./styles/App.css";
 function App() {
 
   const [posts, setPosts] = useState([
-      { id: 1, name: 'JavaScript 1', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-      { id: 2, name: 'JavaScript 2', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-      { id: 3, name: 'JavaScript 3', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-    ]);
+    { id: 1, name: 'JavaScript 1', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
+    { id: 2, name: 'JavaScript 2', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
+    { id: 3, name: 'JavaScript 3', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
+  ]);
 
-    const [posts2, setPosts2] = useState([
-      { id: 1, name: 'Python 1', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-      { id: 2, name: 'Python 2', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-      { id: 3, name: 'Python 3', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-    ]);
+  const [title, setTitle] = useState(' ');
+  const [description, setDescription] = useState(' ');
+
+  // const bodyInputRef = useRef();
+  const addNewPost = (e) => {
+
+    e.preventDefault();
+    console.log(title)
+    console.log(description)
+  
+    // console.log(bodyInputRef.current.value)
+  };
 
   return (
     <div className="App">
-     <PostList posts={posts} title = {'Posts JavaScript'}/>
-     <PostList posts={posts} title = {'Post Python'}/>
+      <form >
+        {/* Управляемый компонент */}
+        <MyInput
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+          type='text'
+          placeholder='Название поста' />
+        <MyInput
+          value={description}
+          onChange={event=>setDescription(event.target.value)}
+          type='text'
+          placeholder='Описание поста' />
+
+
+
+        {/* <input type="text" ref={bodyInputRef} /> */}
+        {/* Неуправляемый/неконтролируемый */}
+        {/* <MyInput
+          ref={bodyInputRef}
+          type='text'
+          placeholder='Описание поста' /> */}
+        <MyButton disabled={false} onClick={addNewPost}>Создать</MyButton>
+      </form>
+      <PostList posts={posts} title={'Post Python'} />
     </div>
   );
 }
---
+++ src/components/UI/button/MyButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './MyButton.module.css'
+const MyButton = ({children, ...props}) => {
+    return (
+        // <button disabled={disabled} className={classes.myButton}>
+        //     {children}
+        // </button> 
+        <button {...props} className={classes.myButton}>
+            {children}
+        </button>
+    );
+};
+
+export default MyButton;
