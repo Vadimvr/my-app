1f3119e1b4bdc4ed9f16791de4e6745017f3d3ee
Вадим <r.vadim.90@gmail.com>
2022-05-02 20:54:21 +0300

    Added postlist

---
+++ src/components/PostList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PostItem from './PostItem';
+
+const PostList = ({posts, title}) => {
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>{title}</h1>
+            {posts.map((item) =>
+                    <PostItem post={item} key={item.id} />
+            )}
+        </div>
+    );
+};
+
+export default PostList;
