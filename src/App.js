2a73b65aaaba81e5cf060727143d43bf2bd8e94e
Вадим <r.vadim.90@gmail.com>
2022-05-02 20:50:33 +0300

    added PostLst.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 // import ClassCounter from "./components/ClassCounter";
 // import Counter from "./components/Counter";
 import "./styles/App.css";
 function App() {
 
-  const [posts, setPosts] = useState(
-    [
+  const [posts, setPosts] = useState([
       { id: 1, name: 'JavaScript 1', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
       { id: 2, name: 'JavaScript 2', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
       { id: 3, name: 'JavaScript 3', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-      { id: 4, name: 'JavaScript 4', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-      { id: 5, name: 'JavaScript 5', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-      { id: 6, name: 'JavaScript 6', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
-    ]
-  );
+    ]);
+
+    const [posts2, setPosts2] = useState([
+      { id: 1, name: 'Python 1', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
+      { id: 2, name: 'Python 2', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
+      { id: 3, name: 'Python 3', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, hic." },
+    ]);
 
   return (
     <div className="App">
-      {posts.map(
-        item =>
-          <PostItem post={item} key = {item.id}/>
-      )};
+     <PostList posts={posts} title = {'Posts JavaScript'}/>
+     <PostList posts={posts} title = {'Post Python'}/>
     </div>
-  );
+  )
 }
 
 export default App;
