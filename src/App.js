c18d3ceccf2fd649e375b8afa21dbae97b6de01f
Vadzim <40643436+Vadimvr@users.noreply.github.com>
2022-05-06 00:52:50 +0300

    Added page number

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import Loader from "./components/UI/Loader/Loader";
 import MyModal from "./components/UI/MyModal/MyModal";
+import { getPageCount, getPagesArray } from "./components/utils/pages";
 import "./styles/App.css";
-
 function App() {
 
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({ sorting: '', query: '' });
     const [modal, setModal] = useState(false)
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
     const sortedAndSearchPosts = usePosts(posts, filter.sorting, filter.query);
+
+    let pagesArray = getPagesArray(totalPages);
     const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-        const posts = await PostService.getAll();
-        setPosts(posts);
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data);
+        const totalCount = response.headers['x-total-count'];
+        setTotalPages(getPageCount(totalCount, limit));
+
     });
 
-
+    console.log(pagesArray);
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
     };
@@ -57,6 +65,16 @@ function App() {
                 </div>
                 : <PostList posts={sortedAndSearchPosts} title={'Posts'} remove={removePost} />
             }
+            <div className="page__wrapper">
+                {pagesArray.map(p =>
+                    <span 
+                    key={p} 
+                    className={page === p ? 'page page__current' : 'page'}
+                    onClick = {()=>setPage(p)}>
+                        {p}
+                    </span>
+                )}
+            </div>
         </div>
     );
 }
